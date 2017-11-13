3742244152273d2f51bfa54e462af77069dbe383
Johnny Boursiquot <jboursiquot@gmail.com>
2017-11-13 18:30:22 -0500

    A context example

--- things.go
+++ things.go
@@ -1,6 +1,11 @@
 package things
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
 
 // Thing is a thing.
 type Thing struct {
@@ -9,4 +14,36 @@ type Thing struct {
 
 func (t Thing) String() string {
 	return fmt.Sprintf("Hello, I'm %s", t.Name)
+}
+
+// DoHardThings will do hard things for a while or until you cancel the context.
+func DoHardThings(ctx context.Context) <-chan struct{} {
+	log.Println("things.DoHardThings >> called...")
+
+	// to simulate hard work for 2 seconds
+	d := 2 * time.Second
+	completeAfter := time.After(d)
+
+	ch := make(chan struct{})
+	thingCtx, cancelFunc := context.WithCancel(ctx)
+	defer cancelFunc()
+
+	for {
+		select {
+		case <-thingCtx.Done():
+			err := thingCtx.Err()
+			if err == context.Canceled {
+				log.Println("things.DoHardThings >> context was canceled")
+			} else if err == context.DeadlineExceeded {
+				log.Println("things.DoHardThings >> context deadline was exceeded")
+			}
+			return ch
+		case <-completeAfter:
+			log.Printf("things.DoHardThings >> did hard things for %v", d)
+			return ch
+		default:
+			log.Println("things.DoHardThings >> doing hard things...")
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
